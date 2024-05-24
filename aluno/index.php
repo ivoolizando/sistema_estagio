9474e60719744bc502a66509572764711adedb71
Ivo Miranda <114839409+ivoolizando@users.noreply.github.com>
2024-05-24 01:35:25 -0300

    ajuste no index

--- aluno/index.php
+++ aluno/index.php
@@ -7,11 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<?php
-include("componentes/header.php");
- ?>
 
 <body>
+    <?php
+    session_start();
+    include("componentes/header.php");
+    ?>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
